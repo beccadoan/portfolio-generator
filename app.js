93a600ee827d5f1fe4aa5230ef3444137684d235
Becca Doan <rebecca.doan@yahoo.com>
2022-07-02 17:27:17 -0500

    added inquirer, and npm install

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2)
-const [myname, github] = profileDataArgs;
+import inquirer from 'inquirer';
 
-fs.writeFile('./index.html', generatePage(myname, github), err => {
-    if (err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+// const pageHTML = generatePage(myname, github)
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            myname: 'name',
+            message: 'What is your name?'
+        }
+    ])
+};
+
+promptUser().then(answers => console.log(answers))

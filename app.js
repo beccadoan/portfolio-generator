9649c2bc18e498a75b0d3a6ea8af95621f253046
Becca Doan <rebecca.doan@yahoo.com>
2022-07-02 19:38:49 -0500

    finished creating dynamic html elements using javascript

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import inquirer from 'inquirer';
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+import fs from 'fs';
+import generatePage from './src/page-template.js'
 
 // const pageHTML = generatePage(myname, github)
 
@@ -141,6 +140,12 @@ const promptProject = portfolioData => {
   promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData)
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        })
     });
     

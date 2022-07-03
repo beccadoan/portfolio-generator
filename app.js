7e976a064640fd894e9d5c7d84bc7c74803000d1
Becca Doan <rebecca.doan@yahoo.com>
2022-07-02 20:22:21 -0500

    completed full project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import inquirer from 'inquirer';
-import fs from 'fs';
 import generatePage from './src/page-template.js'
+import { writeFile, copyFile } from './utils/generate-site.js'
 
 // const pageHTML = generatePage(myname, github)
 
@@ -138,14 +138,21 @@ const promptProject = portfolioData => {
   }
   
   promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData)
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw err;
-
-            console.log('Portfolio complete! Check out index.html to see the output!');
-        })
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
     
